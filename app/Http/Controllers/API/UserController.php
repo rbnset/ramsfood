1371fd97cabb2f6f8f21dda2191d0a7b00f28723
rbn_setiyawan <rbn.setiyawan@gmail.com>
2022-11-23 10:37:24 +0700

    Buat API Register

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Actions\Fortify\PasswordValidationRules;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use App\Models\User;
@@ -12,6 +13,9 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+
+    use PasswordValidationRules;
+
     public function login(Request $request)
     {
 
@@ -48,6 +52,44 @@ class UserController extends Controller
                 'message' => 'Something went wrong',
                 'error' => $error
             ], 'Autentication Filed', 500);
+        }
+    }
+
+    public function register(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => $this->passwordRules()
+
+            ]);
+
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'addres' => $request->addres,
+                'addres' => $request->addres,
+                'houseNumber' => $request->houseNumber,
+                'phoneNumber' => $request->phoneNumber,
+                'city' => $request->city,
+                'password' => Hash::make($request->password),
+            ]);
+
+            $user = User::where('email', $request->email)->first();
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Baerer',
+                'user' => $user
+            ]);
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Someting went wrong',
+                'error' => $error
+            ], 'Autentication Failed', 500);
         }
     }
 }
